b236c98cef47e2293da19615e618c868dc3cf80f
PRAKULNAYAK-GITHUB <prakulnayak2004@gmail.com>
2026-05-24 13:51:37 +0530

    modified:   prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "node -r dotenv/config node_modules/tsx/dist/cli.mjs prisma/seed.ts",
   },
   datasource: {
     url: process.env["DATABASE_URL"],
